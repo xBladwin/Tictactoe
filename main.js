d10ce0f3fdcdc9c831fe8550126ca5602fd584c1
Jade Baldwin <wjbaldwin171@stevenscollege.edu>
2018-11-12 15:42:48 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+two players X & O
+only one winner
+place one symbol per turn, one empty space
+players take turns
+three in a row wins
+  up + down - tr to bl
+  left + right - tl to br
+if full no winner, restart till death
+randomly choose who goes first
+
+- place symbol by clicking a square
+- or with the keyboard
+- look good
+*/
+<<<<<<< HEAD
+
+/*const players = ["player 1", "player 2"];
+let randomValue = players[Math.floor(Math.random() * players.length)]; {
+  console.log('You\'re ' + randomValue)
+}
+*/
+=======
+const players = ['Player 1', 'Player 2'];
+let randomValue = players[Math.floor(Math.random() * players.length)]; {
+  console.log(randomValue);
+}
+>>>>>>> a10dab765a094e05414458477cf184d890a2e104
